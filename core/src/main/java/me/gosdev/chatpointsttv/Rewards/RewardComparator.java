7a5558580117255f504834909c8ca5767763639a
GospelBG <39804434+GospelBG@users.noreply.github.com>
2024-06-24 21:55:08 +0200

    Create RewardComparator.java

---
+++ core/src/main/java/me/gosdev/chatpointsttv/Rewards/RewardComparator.java
@@ -0,0 +1,25 @@
+package me.gosdev.chatpointsttv.Rewards;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import me.gosdev.chatpointsttv.Rewards.Rewards.rewardType;
+
+public class RewardComparator implements Comparator<Reward> {
+    @Override
+    public int compare(Reward o1, Reward o2) {
+        if (o1.getType() != o2.getType()) throw new java.lang.UnsupportedOperationException("Cannot compare " + o1.getType().toString() + " rewards with " + o2.getType().toString());
+        if (o1.getType() == rewardType.CHANNEL_POINTS || o1.getType() == rewardType.FOLLOW || o1.getType() == rewardType.SUB) throw new UnsupportedOperationException("Cannot sort " + o1.getType().toString() + " rewards.");
+        if (o2.getType() == rewardType.CHANNEL_POINTS || o2.getType() == rewardType.FOLLOW || o2.getType() == rewardType.SUB) throw new UnsupportedOperationException("Cannot sort " + o1.getType().toString() + " rewards.");
+
+        List<Integer> amounts = new ArrayList<Integer>();
+        Rewards.getRewards(rewardType.CHEER).forEach((reward) -> {
+            amounts.add(Integer.parseInt(reward.getEvent()));
+        });
+
+
+        return Integer.parseInt(o1.getEvent()) - Integer.parseInt(o2.getEvent());
+    }
+
+}
