bffa278c4c5dc62402271bd6e3239cf006ee4b93
GospelBG <39804434+GospelBG@users.noreply.github.com>
2024-06-25 17:16:37 +0200

    Fix rewards map

--- core/src/main/java/me/gosdev/chatpointsttv/Rewards/Rewards.java
+++ core/src/main/java/me/gosdev/chatpointsttv/Rewards/Rewards.java
@@ -1,18 +1,16 @@
 package me.gosdev.chatpointsttv.Rewards;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
 
 import me.gosdev.chatpointsttv.ChatPointsTTV;
 public class Rewards {
-    private static FileConfiguration config = ChatPointsTTV.getPlugin().config;
     public static enum rewardType {
         FOLLOW,
         CHANNEL_POINTS,
@@ -21,22 +19,17 @@ public class Rewards {
         GIFT
     };
 
-    public static Map<rewardType, ArrayList<Reward>> rewards = Collections.emptyMap();
-
-    public static Boolean isEnabled(rewardType type) {
-        return true;
-        //if (config.getConfigurationSection("CHANNEL_POINTS_REWARDS") != null | config.getConfigurationSection("CHANNEL_POINTS_REWARDS").startsWith("MemorySection[path="))
-    }
+    public static Map<rewardType, ArrayList<Reward>> rewards = new HashMap<rewardType, ArrayList<Reward>>();
 
     public static ArrayList<Reward> getRewards(rewardType type) {
         if (rewards.get(type) != null) return rewards.get(type); // Give stored dictionary if it was already fetched
 
-        ConfigurationSection config_value = config.getConfigurationSection(type.toString().toUpperCase() + "_REWARDS");
-        ArrayList<Reward> rewards = new ArrayList<>();
+        ConfigurationSection config_value = ChatPointsTTV.getPlugin().config.getConfigurationSection(type.toString().toUpperCase() + "_REWARDS");
+        ArrayList<Reward> reward_list = new ArrayList<>();
         if (type == rewardType.FOLLOW) {
-            List<String> follow_rewards = (List<String>) config.getList(type.toString().toUpperCase() + "_REWARDS");
+            List<String> follow_rewards = (List<String>) ChatPointsTTV.getPlugin().config.getList(type.toString().toUpperCase() + "_REWARDS");
             if (follow_rewards == null || follow_rewards.isEmpty()) return null;
-            rewards.add(new Reward(type, null, follow_rewards));
+            reward_list.add(new Reward(type, null, follow_rewards));
         } else {
             Set<String> keys = config_value.getKeys(false);
             if (config_value == null || keys.size() == 0) return null;
@@ -46,10 +39,11 @@ public class Rewards {
 
                 //log.info(key);
                 //log.info(config_value.getList(key).toString());
-                rewards.add(new Reward(type, key, (List<String>) config_value.getList(key)));
+                reward_list.add(new Reward(type, key, (List<String>) config_value.getList(key)));
             }
         }
 
-        return rewards;
+        rewards.put(type, reward_list);
+        return reward_list;
     }
 }
