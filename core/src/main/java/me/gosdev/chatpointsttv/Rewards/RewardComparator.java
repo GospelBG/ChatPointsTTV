0a83ee4bfb04ef7407e79f83b260fdd0c1fe3170
GospelBG <39804434+GospelBG@users.noreply.github.com>
2024-09-18 23:53:40 +0200

    Prioritized Sorting

--- core/src/main/java/me/gosdev/chatpointsttv/Rewards/RewardComparator.java
+++ core/src/main/java/me/gosdev/chatpointsttv/Rewards/RewardComparator.java
@@ -2,20 +2,22 @@ package me.gosdev.chatpointsttv.Rewards;
 
 import java.util.Comparator;
 
-import me.gosdev.chatpointsttv.Rewards.Rewards.rewardType;
-
 public class RewardComparator implements Comparator<Reward> {
     @Override
     public int compare(Reward o1, Reward o2) {
         if (o1.getType() != o2.getType()) throw new java.lang.UnsupportedOperationException("Cannot compare " + o1.getType().toString() + " rewards with " + o2.getType().toString());
 
-        if (o1.getChannel().equals(Rewards.EVERYONE) && !o2.getChannel().equals(Rewards.EVERYONE)) return 1;
-        if (o2.getChannel().equals(Rewards.EVERYONE) && !o1.getChannel().equals(Rewards.EVERYONE)) return -1;
+        try {
+            int difference = Integer.parseInt(o2.getEvent()) - Integer.parseInt(o1.getEvent());
 
-        if (o1.getType() == rewardType.CHANNEL_POINTS || o1.getType() == rewardType.FOLLOW || o1.getType() == rewardType.SUB) return 0;
-        if (o2.getType() == rewardType.CHANNEL_POINTS || o2.getType() == rewardType.FOLLOW || o2.getType() == rewardType.SUB) return 0;
+            if (difference == 0) throw new NumberFormatException(); // If value matches compare target channel (go straight to catch)
+            return difference;
+        } catch (NumberFormatException e) {
+            if (o1.getChannel().equals(Rewards.EVERYONE) && !o2.getChannel().equals(Rewards.EVERYONE)) return 1;
+            if (o2.getChannel().equals(Rewards.EVERYONE) && !o1.getChannel().equals(Rewards.EVERYONE)) return -1;
+            return 0;
+        }
 
-        return Integer.parseInt(o2.getEvent()) - Integer.parseInt(o1.getEvent());
     }
 
 }
