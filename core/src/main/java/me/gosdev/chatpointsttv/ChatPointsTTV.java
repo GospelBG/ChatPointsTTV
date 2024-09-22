ea34dca62e98b34d6ea43feba1de71695d493cba
GospelBG <39804434+GospelBG@users.noreply.github.com>
2024-09-22 17:04:31 +0200

    Unregister events when disabling
    /twitch reload command would duplicate join messages

--- core/src/main/java/me/gosdev/chatpointsttv/ChatPointsTTV.java
+++ core/src/main/java/me/gosdev/chatpointsttv/ChatPointsTTV.java
@@ -22,6 +22,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -281,6 +282,8 @@ public class ChatPointsTTV extends JavaPlugin {
 
         Rewards.rewards = new HashMap<rewardType,ArrayList<Reward>>();
         TwitchEventHandler.rewardBold = null;
+
+        HandlerList.unregisterAll(this);
     }
 
     public void linkToTwitch(CommandSender p, String token) {
