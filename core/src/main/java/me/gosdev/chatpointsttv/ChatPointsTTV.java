81fa9d721b2cea2e75926ca2845c401d882f81e1
GospelBG <39804434+GospelBG@users.noreply.github.com>
2024-12-26 19:47:21 +0100

    Raid Events

--- core/src/main/java/me/gosdev/chatpointsttv/ChatPointsTTV.java
+++ core/src/main/java/me/gosdev/chatpointsttv/ChatPointsTTV.java
@@ -53,6 +53,7 @@ import net.md_5.bungee.api.chat.TextComponent;
 import com.github.twitch4j.helix.domain.StreamList;
 import com.github.twitch4j.helix.domain.User;
 import com.github.twitch4j.helix.domain.UserList;
+import com.github.twitch4j.pubsub.events.RaidGoEvent;
 import com.github.twitch4j.pubsub.events.RewardRedeemedEvent;
 
 import me.gosdev.chatpointsttv.Rewards.Rewards;
@@ -434,6 +435,18 @@ public class ChatPointsTTV extends JavaPlugin {
                     }
                 });
             }
+            if (Rewards.getRewards(Rewards.rewardType.RAID) != null) {
+                eventManager.onEvent(RaidGoEvent.class, new Consumer<RaidGoEvent>() { // Don't count for CountdownLatch because this is PubSub
+                    @Override
+                    public void accept(RaidGoEvent e) {
+                        try {
+                            eventHandler.onRaid(e);
+                        } catch (NullPointerException ex) {}// May get NullPointerException if event is triggered while still subscribing
+                        catch (Exception ex) {
+                        }
+                    }
+                }); 
+            }
             if (config.getBoolean("SHOW_CHAT")) {
                 eventManager.onEvent(ChannelMessageEvent.class, event -> {
                     if (!chatBlacklist.contains(event.getUser().getName())) {
@@ -518,6 +531,10 @@ public class ChatPointsTTV extends JavaPlugin {
 
         if (Rewards.getRewards(Rewards.rewardType.SUB) != null || Rewards.getRewards(Rewards.rewardType.GIFT) != null) {
             eventSocket.register(SubscriptionTypes.CHANNEL_CHAT_NOTIFICATION.prepareSubscription(b -> b.broadcasterUserId(channel_id).userId(user_id).build(), null));
+        }
+
+        if (Rewards.getRewards(Rewards.rewardType.RAID) != null) {
+            client.getPubSub().listenForRaidEvents(oauth, channel_id);
         }
         utils.sendMessage(Bukkit.getConsoleSender(), "Listening to " + channel + "'s events...");
         client.getChat().joinChannel(channel);
