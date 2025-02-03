2ccf7a4bc97e061aeb6a76732af0e836fe1f80e8
GospelBG <39804434+GospelBG@users.noreply.github.com>
2025-02-04 00:17:16 +0100

    Libby fix

--- core/src/main/java/me/gosdev/chatpointsttv/Utils/LibraryLoader.java
+++ core/src/main/java/me/gosdev/chatpointsttv/Utils/LibraryLoader.java
@@ -2,9 +2,13 @@ package me.gosdev.chatpointsttv.Utils;
 
 import java.util.ArrayList;
 
+import org.bukkit.ChatColor;
+
+import com.alessiodp.libby.BukkitLibraryManager;
+import com.alessiodp.libby.Library;
+import com.alessiodp.libby.logging.LogLevel;
+
 import me.gosdev.chatpointsttv.ChatPointsTTV;
-import net.byteflux.libby.BukkitLibraryManager;
-import net.byteflux.libby.Library;
 
 public class LibraryLoader {
 
@@ -14,24 +18,29 @@ public class LibraryLoader {
     public static ArrayList<Library> libraries = new ArrayList<Library>() {
         {
             add(Library.builder()
-                .groupId("com{}github{}twitch4j")
-                .artifactId("twitch4j")
-                .version("1.23.0")
-                .relocate("com{}github{}twitch4j{}twitch4j", relocationBase + "twitch4j")
-                .build());
-
-        add(Library.builder()
-                .groupId("com{}github{}philippheuer{}events4j")
-                .artifactId("events4j-handler-simple")
-                .version("0.12.2")
-                .relocate("com{}github{}philippheuer{}events4j", relocationBase + "events4j")
-                .build());
-
-        add(Library.builder()
                 .groupId("org{}json")
                 .artifactId("json")
                 .version("20240303")
                 .relocate("org{}json{}json", relocationBase + "json")
+                .resolveTransitiveDependencies(true)
+                .build());
+
+            // Define the main library with relocation
+            add(Library.builder()
+                .groupId("com{}github{}philippheuer.events4j")
+                .artifactId("events4j-handler-simple")
+                .version("0.12.2")
+                .relocate("com{}github{}philippheuer.events4j", relocationBase + "events4j")
+                .resolveTransitiveDependencies(true)
+                .build());
+                
+            add(Library.builder()
+                .groupId("com{}github{}twitch4j")
+                .artifactId("twitch4j")
+                .version("1.23.0")
+                .resolveTransitiveDependencies(true)
+                .relocate("com{}github{}twitch4j{}twitch4j", relocationBase + "twitch4j")
+                .relocate("com{}fasterxml{}jackson", relocationBase + "jackson")
                 .build());
         }
     };
