2c886debb748527440b28399ea8b86a40b46108e
GospelBG <39804434+GospelBG@users.noreply.github.com>
2025-03-19 14:34:31 +0100

    Improved accounts command

--- src/main/java/me/gosdev/chatpointsttv/Commands/AccountsCommand.java
+++ src/main/java/me/gosdev/chatpointsttv/Commands/AccountsCommand.java
@@ -14,15 +14,27 @@ import net.md_5.bungee.api.chat.TextComponent;
 
 public class AccountsCommand {
     public static void displayAccounts(CommandSender p) {
-        TextComponent msg = new TextComponent("Connected Accounts:\n");
+        TextComponent msg = new TextComponent("---------- " + ChatColor.LIGHT_PURPLE + ChatColor.BOLD  + "Connected Accounts" + ChatColor.RESET + " ----------\n\n");
 
         for (Channel channel : ChatPointsTTV.getPlugin().getTwitch().getListenedChannels().values()) {
-            BaseComponent deleteButton = new ComponentBuilder(ChatColor.RED + "[x]").create()[0];
+            BaseComponent deleteButton = new ComponentBuilder(ChatColor.RED + "  [x]").create()[0];
             deleteButton.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to unlink this account").create()));
             deleteButton.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/twitch unlink " + channel.getChannelUsername()));
             msg.addExtra(deleteButton);
             msg.addExtra(new TextComponent(" " + channel.getChannelUsername() + "\n"));
         }
+
+        TextComponent addBtn = new TextComponent(ChatColor.GREEN + "" + ChatColor.BOLD + "\n[+]" + ChatColor.RESET + ChatColor.GREEN + " Add account");
+        addBtn.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to link another account").create()));
+        addBtn.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/twitch link"));
+
+        TextComponent stopBtn = new TextComponent(ChatColor.RED + "" + ChatColor.BOLD + "[x]" + ChatColor.RESET + ChatColor.RED + " Remove all accounts");
+        stopBtn.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to unlink all accounts").create()));
+        stopBtn.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/twitch unlink"));
+
+        msg.addExtra(addBtn);
+        msg.addExtra(ChatColor.GRAY + " - ");
+        msg.addExtra(stopBtn);
 
         p.spigot().sendMessage(msg);
     }
