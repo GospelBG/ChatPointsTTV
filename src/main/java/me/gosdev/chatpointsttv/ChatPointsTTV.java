2443ecb3ee7dd433ec075429019fdbd5472fce84
GospelBG <39804434+GospelBG@users.noreply.github.com>
2025-03-19 01:01:44 +0100

    Reset color after prefix

--- src/main/java/me/gosdev/chatpointsttv/ChatPointsTTV.java
+++ src/main/java/me/gosdev/chatpointsttv/ChatPointsTTV.java
@@ -45,7 +45,7 @@ public class ChatPointsTTV extends JavaPlugin {
     public FileConfiguration config;
     public Metrics metrics;
 
-    public final static String msgPrefix = ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "[ChatPointsTTV] " + ChatColor.RESET;
+    public final static String msgPrefix = ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "[ChatPointsTTV] " + ChatColor.WHITE + "" + ChatColor.RESET;
 
 
     public static enum permissions {
