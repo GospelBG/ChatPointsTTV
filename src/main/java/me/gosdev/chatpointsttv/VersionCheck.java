5ad1343e4bb3ed34d8c19acabaee9b78eb151fb0
GospelBG <39804434+GospelBG@users.noreply.github.com>
2025-04-06 16:10:23 +0200

    Hotfix: Fix version check

--- src/main/java/me/gosdev/chatpointsttv/VersionCheck.java
+++ src/main/java/me/gosdev/chatpointsttv/VersionCheck.java
@@ -7,22 +7,16 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.bukkit.entity.Player;
 import org.json.JSONArray;
 
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 
 public class VersionCheck {
     private final static String url = "https://api.modrinth.com/v2/project/nN0gRvoO/version";
 
     public final static String download_url = "https://modrinth.com/plugin/chatpointsttv";
     public static String latestVersion;
-    public static Boolean runningLatest = false;
+    public static Boolean runningLatest = true;
     
     public static void check() {
         try {
@@ -35,26 +29,12 @@ public class VersionCheck {
                 }
             }
             conn.disconnect();
-
             JSONArray json = new JSONArray(result.toString());
             latestVersion = json.getJSONObject(0).getString("version_number");
 
-            if (!ChatPointsTTV.getPlugin().getDescription().getVersion().equals(latestVersion.replaceAll("[^\\d.]", ""))) {
+            if (!ChatPointsTTV.getPlugin().getDescription().getVersion().equals(latestVersion)) {
                 runningLatest = false;
-                for (Player p: ChatPointsTTV.getPlugin().getServer().getOnlinePlayers()) {
-                    if (p.hasPermission(ChatPointsTTV.permissions.MANAGE.permission_id)) {
-                        ComponentBuilder formatted = new ComponentBuilder(ChatColor.YELLOW + "Click " + ChatColor.UNDERLINE + "here" + ChatColor.RESET + ChatColor.YELLOW + " to download the latest version");
-            
-                        BaseComponent btn = formatted.create()[0];
-                        btn.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to open in browser").create())); 
-                        btn.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, download_url));
-
-                        p.spigot().sendMessage(new TextComponent(ChatColor.YELLOW + "ChatPointsTTV v" + latestVersion + " has been released!"));
-                        p.spigot().sendMessage(btn);
-                    }
-                }
                 ChatPointsTTV.log.info(ChatColor.YELLOW + "ChatPointsTTV v" + latestVersion + " has been released! Download the latest version in " + download_url);
-
             }
 
         } catch (IOException | URISyntaxException e) {
