4b3cd057dbc0a027127ec76400ca046c3f31f830
GospelBG <39804434+GospelBG@users.noreply.github.com>
2025-04-23 04:57:31 +0200

    Replace hardcoded strings with JSONObjects

--- src/main/java/me/gosdev/chatpointsttv/Twitch/EventTest.java
+++ src/main/java/me/gosdev/chatpointsttv/Twitch/EventTest.java
@@ -3,6 +3,8 @@ package me.gosdev.chatpointsttv.Twitch;
 import java.time.Instant;
 import java.util.Optional;
 
+import org.json.JSONObject;
+
 import com.github.twitch4j.common.enums.SubscriptionPlan;
 import static com.github.twitch4j.common.util.TypeConvert.jsonToObject;
 import com.github.twitch4j.eventsub.events.ChannelChatMessageEvent;
@@ -11,117 +13,129 @@ import com.github.twitch4j.eventsub.events.ChannelFollowEvent;
 import com.github.twitch4j.eventsub.events.ChannelRaidEvent;
 import com.github.twitch4j.eventsub.events.CustomRewardRedemptionAddEvent;
 
+import me.gosdev.chatpointsttv.ChatPointsTTV;
+
 public class EventTest {
     public static CustomRewardRedemptionAddEvent ChannelPointsRedemptionEvent(String channel, String chatter, String title, Optional<String> userInput) {
-        CustomRewardRedemptionAddEvent event = jsonToObject(
-            "{\"user_id\":\"" + TwitchUtils.getUserId(chatter) +
-            "\",\"user_login\":\"" + chatter.toLowerCase() +
-            "\",\"user_name\":\"" + chatter +
-            "\",\"broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"broadcaster_user_name\":\"" + channel +
-            "\",\"user_input\":\"" + userInput.orElse("") +
-            "\",\"status\":\"unfulfilled" +
-            "\",\"reward\":{\"id\":\"0" + 
-            "\",\"title\":\"" + title +
-            "\",\"cost\":\"123123\"}}",
-            CustomRewardRedemptionAddEvent.class);
+        JSONObject json = new JSONObject();
+        JSONObject reward = new JSONObject();
 
+        json.put("user_id", TwitchUtils.getUserId(chatter));
+        json.put("user_login", chatter.toLowerCase());
+        json.put("user_name", chatter);
+
+        json.put("broadcaster_user_id", TwitchUtils.getUserId(channel));
+        json.put("broadcaster_user_login", channel.toLowerCase());
+        json.put("broadcaster_user_name", channel);
+        
+        json.put("user_input", userInput.orElse(""));
+        json.put("status", "unfullfilled");
+        reward.put("id", "0");
+        reward.put("title", title);
+        reward.put("cost", "123");
+
+        json.put("reward", reward);
+        ChatPointsTTV.log.info(json.toString());
+        CustomRewardRedemptionAddEvent event = jsonToObject(json.toString(), CustomRewardRedemptionAddEvent.class);
         return event;
     }
 
     public static ChannelFollowEvent FollowEvent(String channel, String chatter) {
-        ChannelFollowEvent event = jsonToObject(
-            "{\"user_id\":\"" + TwitchUtils.getUserId(chatter) +
-            "\",\"user_login\":\"" + chatter.toLowerCase() +
-            "\",\"user_name\":\"" + chatter +
-            "\",\"broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"broadcaster_user_name\":\"" + channel +
-            "\",\"followed_at\":\"" + Instant.now().toString() +"\"}",
-        ChannelFollowEvent.class);
+        JSONObject json = new JSONObject();
 
+        json.put("user_id", TwitchUtils.getUserId(chatter));
+        json.put("user_login", chatter.toLowerCase());
+        json.put("user_name", chatter);
+
+        json.put("broadcaster_user_id", TwitchUtils.getUserId(channel));
+        json.put("broadcaster_user_login", channel.toLowerCase());
+        json.put("broadcaster_user_name", channel);
+
+        json.put("followed_at", Instant.now().toString());
+
+        ChannelFollowEvent event = jsonToObject(json.toString(), ChannelFollowEvent.class);
         return event;
     }
 
-    public static ChannelChatMessageEvent CheerEvent(String channel, String chatter, int amount) {
-        ChannelChatMessageEvent event = jsonToObject(
-            "{\"chatter_user_id\":\"" + TwitchUtils.getUserId(chatter) +
-            "\",\"chatter_user_login\":\"" + chatter.toLowerCase() +
-            "\",\"chatter_user_name\":\"" + chatter +
-            "\",\"broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"broadcaster_user_name\":\"" + channel +
-            "\",\"cheer\": {\"bits\": " + amount +"}}", 
-        ChannelChatMessageEvent.class);
-        
+    public static ChannelChatMessageEvent CheerEvent(String channel, String chatter, Integer amount) {
+        JSONObject json = new JSONObject();
+
+        json.put("chatter_user_id", TwitchUtils.getUserId(chatter));
+        json.put("chatter_user_login", chatter.toLowerCase());
+        json.put("chatter_user_name", chatter);
+
+        json.put("broadcaster_user_id", TwitchUtils.getUserId(channel));
+        json.put("broadcaster_user_login", channel.toLowerCase());
+        json.put("broadcaster_user_name", channel);
+
+        json.put("cheer", new JSONObject().put("bits", amount.toString()));
+
+        ChannelChatMessageEvent event = jsonToObject(json.toString(), ChannelChatMessageEvent.class);
         return event;
     }
 
-    public static ChannelChatNotificationEvent SubEvent(String channel, String chatter, SubscriptionPlan plan, int months) {
-        ChannelChatNotificationEvent event = jsonToObject(
-            "{\"chatter_user_id\":\"" + TwitchUtils.getUserId(chatter) +
-            "\",\"chatter_user_login\":\"" + chatter.toLowerCase() +
-            "\",\"chatter_user_name\":\"" + chatter +
-            "\",\"broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"broadcaster_user_name\":\"" + channel +
-            "\",\"notice_type\": \"sub" +
-            "\",\"sub\":{\"sub_tier\":\"" + (plan.equals(SubscriptionPlan.TWITCH_PRIME) ? "1000" : plan.toString()) + 
-            "\",\"is_prime\":" + (plan.equals(SubscriptionPlan.TWITCH_PRIME) ? "true" : "false") +
-            ",\"duration_months\":" + months + "}}",
-            ChannelChatNotificationEvent.class);
+    public static ChannelChatNotificationEvent SubEvent(String channel, String chatter, SubscriptionPlan plan, Integer months) {
+        JSONObject json = new JSONObject();
+        JSONObject sub = new JSONObject();
 
+        json.put("chatter_user_id", TwitchUtils.getUserId(chatter));
+        json.put("chatter_user_login", chatter.toLowerCase());
+        json.put("chatter_user_name", chatter);
+
+        json.put("broadcaster_user_id", TwitchUtils.getUserId(channel));
+        json.put("broadcaster_user_login", channel.toLowerCase());
+        json.put("broadcaster_user_name", channel);
+
+        json.put("notice_type", "sub");
+
+        sub.put("sub_tier", plan.equals(SubscriptionPlan.TWITCH_PRIME) ? "1000" : plan.toString());
+        sub.put("is_prime", plan.equals(SubscriptionPlan.TWITCH_PRIME) ? "true" : "false");
+        sub.put("duration_months", months.toString());
+
+        json.put("sub", sub);
+
+        ChannelChatNotificationEvent event = jsonToObject(json.toString(), ChannelChatNotificationEvent.class);
         return event;
     }
 
-    public static ChannelChatNotificationEvent ResubEvent(String channel, String chatter, SubscriptionPlan plan, int months) {
-        ChannelChatNotificationEvent event = jsonToObject(
-            "{\"chatter_user_id\":\"" + TwitchUtils.getUserId(chatter) +
-            "\",\"chatter_user_login\":\"" + chatter.toLowerCase() +
-            "\",\"chatter_user_name\":\"" + chatter +
-            "\",\"broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"broadcaster_user_name\":\"" + channel +
-            "\",\"notice_type\": \"sub\"" +
-            "\",resub\":{\"sub_tier\":\"" + plan.name() + 
-            "\",\"is_prime\":" + (plan.equals(SubscriptionPlan.TWITCH_PRIME) ? "true" : "false") +
-            "\",is_gift\": \"false\"" +
-            "\",duration_months\":" + months + 
-            "\",cumulative_months\":" + months + 
-            "\",streak_months\":" + months + "}}",
-            ChannelChatNotificationEvent.class);
+    public static ChannelChatNotificationEvent SubGiftEvent(String channel, String chatter, Integer amount) {
+        JSONObject json = new JSONObject();
+        JSONObject sub = new JSONObject();
 
+
+        json.put("chatter_user_id", TwitchUtils.getUserId(chatter));
+        json.put("chatter_user_login", chatter.toLowerCase());
+        json.put("chatter_user_name", chatter);
+
+        json.put("broadcaster_user_id", TwitchUtils.getUserId(channel));
+        json.put("broadcaster_user_login", channel.toLowerCase());
+        json.put("broadcaster_user_name", channel);
+
+        json.put("notice_type", "community_sub_gift");
+
+        sub.put("sub_tier", "1000");
+        sub.put("total", amount);
+
+        json.put("community_sub_gift", sub);
+
+        ChannelChatNotificationEvent event = jsonToObject(json.toString(), ChannelChatNotificationEvent.class);
         return event;
     }
 
-    public static ChannelChatNotificationEvent SubGiftEvent(String channel, String chatter, int amount) {
-        ChannelChatNotificationEvent event = jsonToObject(
-            "{\"chatter_user_id\":\"" + TwitchUtils.getUserId(chatter) +
-            "\",\"chatter_user_login\":\"" + chatter.toLowerCase() +
-            "\",\"chatter_user_name\":\"" + chatter +
-            "\",\"broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"broadcaster_user_name\":\"" + channel +
-            "\",\"notice_type\": \"community_sub_gift" +
-            "\",\"community_sub_gift\":{\"sub_tier\":\"1000" + 
-            "\",\"total\":" + amount + "}}",
-            ChannelChatNotificationEvent.class);
+    public static ChannelRaidEvent RaidReward(String channel, String raider, Integer viewers) {
+        JSONObject json = new JSONObject();
 
-            return event;
-    }
+        json.put("from_broadcaster_user_id", TwitchUtils.getUserId(raider));
+        json.put("from_broadcaster_user_login", raider.toLowerCase());
+        json.put("from_broadcaster_user_name", raider);
 
-    public static ChannelRaidEvent RaidReward(String channel, String raider, int viewers) {
-        ChannelRaidEvent event = jsonToObject(
-            "{\"from_broadcaster_user_id\":\"" + TwitchUtils.getUserId(raider) +
-            "\",\"from_broadcaster_user_login\":\"" + raider.toLowerCase() +
-            "\",\"from_broadcaster_user_name\":\"" + raider +
-            "\",\"to_broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"to_broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"to_broadcaster_user_name\":\"" + channel +
-            "\",\"viewers\": \"" + viewers + "\"}",
-            ChannelRaidEvent.class);
+        json.put("to_broadcaster_user_id", TwitchUtils.getUserId(channel));
+        json.put("to_broadcaster_user_login", channel.toLowerCase());
+        json.put("to_broadcaster_user_name", channel);
 
+        json.put("viewers", viewers.toString());
+
+        ChannelRaidEvent event = jsonToObject(json.toString(), ChannelRaidEvent.class);
         return event;
     }
 }
