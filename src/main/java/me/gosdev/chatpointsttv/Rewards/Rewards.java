d7ca28f43419f290b4ded59896d8193c515f9e46
GospelBG <39804434+GospelBG@users.noreply.github.com>
2025-04-23 03:41:37 +0200

    Remade config rewards logic

--- src/main/java/me/gosdev/chatpointsttv/Rewards/Rewards.java
+++ src/main/java/me/gosdev/chatpointsttv/Rewards/Rewards.java
@@ -26,47 +26,62 @@ public class Rewards {
     public static ArrayList<Reward> getRewards(FileConfiguration config, rewardType type) {
         if (rewards.get(type) != null) return rewards.get(type); // Give stored dictionary if it was already fetched
 
-        Object config_obj = config.get(type.toString().toUpperCase() + "_REWARDS");
+        String key = type.toString().toUpperCase() + "_REWARDS";
         ArrayList<Reward> reward_list = new ArrayList<>();
 
-        try {
-            if (config_obj instanceof ArrayList && type.equals(rewardType.FOLLOW)) { // Should only be non-specific Follow rewards
-                reward_list.add(new Reward(type, EVERYONE, null, config.getStringList(type.toString().toUpperCase() + "_REWARDS")));
-            } else if (config_obj instanceof  ConfigurationSection) {
-                ConfigurationSection config_rewards = (ConfigurationSection) config_obj;
-                
-                if (type == rewardType.FOLLOW) { // Follow rewards should have one level less
-                    Set<String> keys = ((ConfigurationSection) config_rewards).getKeys(false);
-                    for (String channel : keys) {
-                        reward_list.add(new Reward(type, channel.equals("default") ? EVERYONE : channel, null, config_rewards.getStringList(channel)));
-                    }
-                } else {
-                    Set<String> keys = ((ConfigurationSection) config_rewards).getKeys(false);
-                    for (String key : keys) {
-                        ConfigurationSection channelSection = config_rewards.getConfigurationSection(key);
+        if (!config.contains(key)) return null; // No configured rewards for this type
+
+        if (type.equals(rewardType.FOLLOW)) {
+            if (config.isConfigurationSection(key)) { // Streamer-specific?
+                Set<String> keys = (config.getConfigurationSection(key)).getKeys(false);
+                for (String channel : keys) {
+                    reward_list.add(new Reward(type, channel.equals("default") ? EVERYONE : channel, null, config.getConfigurationSection(key).getStringList(channel)));
+                }
+            } else if (config.isList(key)) {
+                reward_list.add(new Reward(type, EVERYONE, null, config.getStringList(key)));
+            } else {
+                ChatPointsTTV.log.severe("ChatPointsTTV: Follow actions must be entered as a list (or a configuration section, if targeting specific streamers). Read the docs for more information.");
+                return null;
+            }
+        } else {
+            if (config.isConfigurationSection(key)) {
+                ConfigurationSection section = config.getConfigurationSection(key);
+                Set<String> keys = section.getKeys(false);
+                    for (String subkey : keys) {
+                        if (!section.isConfigurationSection(subkey) && !section.isList(subkey)) {
+                            ChatPointsTTV.log.severe("ChatPointsTTV: Invalid configuration for " + type.toString().toLowerCase() + " (" + subkey + ") actions. Read the docs for more information.");
+                            continue;
+                        }
+                        if (type.equals(rewardType.CHEER) || type.equals(rewardType.GIFT) || type.equals(rewardType.RAID)) {
+                            try {
+                                Integer.valueOf(subkey);
+                            } catch (NumberFormatException e) {
+                                ChatPointsTTV.log.severe("ChatPointsTTV: \"" + subkey +  "\" must be a number.");
+                                continue;
+                            }
+                        }
+
+                        ConfigurationSection channelSection = section.getConfigurationSection(subkey);
                         if (channelSection == null) {
                             // No channel specified
-                            reward_list.add(new Reward(type, EVERYONE, key, config_rewards.getStringList(key)));
+                            reward_list.add(new Reward(type, EVERYONE, subkey, section.getStringList(subkey)));
                         } else {
                             // Streamer specific event
                             Set<String> channelKeys = channelSection.getKeys(false);
                             for (String channel : channelKeys) {
-                                reward_list.add(new Reward(type, channel.equals("default") ? EVERYONE : channel, key, channelSection.getStringList(channel)));
+                                reward_list.add(new Reward(type, channel.equals("default") ? EVERYONE : channel, subkey, channelSection.getStringList(channel)));
                             }
                         }
                     }
-                }
             } else {
-                ChatPointsTTV.log.warning("Invalid config for " + type.toString() + " rewards");
-                return null; 
+                ChatPointsTTV.log.severe("ChatPointsTTV: Invalid configuration for " + type.toString().toLowerCase() + " actions. Read the docs for more information.");
+                return null;
             }
-            reward_list.sort(new RewardComparator());
-            rewards.put(type, reward_list);
-
-            return rewards.get(type);
-        } catch (IllegalArgumentException e) {
-            ChatPointsTTV.log.warning(e.getMessage());
-            return null;
         }
+
+        reward_list.sort(new RewardComparator());
+        rewards.put(type, reward_list);
+
+        return rewards.get(type);
     }
 }
