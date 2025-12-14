e22949030a6676654303d3a4bfc4039a31d3eab7
GosDev <39804434+GospelBG@users.noreply.github.com>
2025-12-15 00:42:45 +0100

    Move parseQuotes() to LocalizationUtils

--- src/main/java/me/gosdev/chatpointsttv/Utils/LocalizationUtils.java
+++ src/main/java/me/gosdev/chatpointsttv/Utils/LocalizationUtils.java
@@ -1,5 +1,7 @@
 package me.gosdev.chatpointsttv.Utils;
 
+import java.util.ArrayList;
+
 import me.gosdev.chatpointsttv.ChatPointsTTV;
 import me.gosdev.chatpointsttv.Platforms;
 import net.md_5.bungee.api.ChatColor;
@@ -33,5 +35,30 @@ public class LocalizationUtils {
         }
 
         return out;
+    }
+
+    public static String[] parseQuotes(String[] in) {
+        ArrayList<String> args =  new ArrayList<>();
+        for (int i = 0; i < in.length; i++) {
+            String arg = in[i];
+            // Check if the argument starts with a quote and does not end with an escaped quote
+            if (arg.startsWith("\"") && !arg.endsWith("\\\"")) {
+            StringBuilder sb = new StringBuilder(arg.substring(1));
+            // Continue appending arguments until the closing quote is found
+            while (i + 1 < in.length && !(in[i + 1].endsWith("\"") && !in[i + 1].endsWith("\\\""))) {
+                sb.append(" ").append(in[++i]);
+            }
+            // Append the last part of the quoted argument
+            if (i + 1 < in.length) {
+                sb.append(" ").append(in[++i], 0, in[i].length() - 1);
+            }
+            // Add the complete quoted argument to the args list
+            args.add(sb.toString().replace("\\\"", "\""));
+            } else {
+            // Add the argument to the args list, replacing escaped quotes
+            args.add(arg.replace("\\\"", "\""));
+            }
+        }
+        return args.toArray(new String[0]);
     }
 }
