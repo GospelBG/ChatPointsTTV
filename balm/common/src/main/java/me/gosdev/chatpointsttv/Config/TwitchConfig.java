dc72d6fecd77ecfdb09a67b0185824ebc88eae8d
GosDev <39804434+GospelBG@users.noreply.github.com>
2026-08-26 05:07:22 +0200

    Fix blacklist in config

--- balm/common/src/main/java/me/gosdev/chatpointsttv/Config/TwitchConfig.java
+++ balm/common/src/main/java/me/gosdev/chatpointsttv/Config/TwitchConfig.java
@@ -4,6 +4,7 @@ import me.gosdev.chatpointsttv.AlertMode;
 import me.gosdev.chatpointsttv.Config.Adapters.BalmAlertMode;
 import net.blay09.mods.balm.platform.config.reflection.Comment;
 import net.blay09.mods.balm.platform.config.reflection.Config;
+import net.blay09.mods.balm.platform.config.reflection.NestedType;
 import net.minecraft.util.StringRepresentable;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class TwitchConfig {
     @Comment("If enabled, follow events from people who had already been following the channel will be ignored.")
     public boolean followSpamProtection = true;
 
+    @NestedType(String.class)
     @Comment("Add chat bot and other unwanted chatter usernames (in lowercase) to this list to prevent their messages displaying on the in-game chat.")
     public List<String> chatBlacklist = List.of();
 
